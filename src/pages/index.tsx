2de027f8ce0ad79a604157a0735ed4215c9141a9
yewe <khalid.yewe@gmail.com>
2022-12-15 10:27:09 +0300

    cleared the home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return <div className="text-2xl text-red-200">Home</div>;
+};
+
+export default Home;
